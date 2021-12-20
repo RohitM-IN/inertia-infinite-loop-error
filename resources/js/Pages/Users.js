b0cadfc159a3af087d12d8fa07cc65cd6163c7c3
RootAndroid58 <rootand58@gmail.com>
2021-12-20 19:10:58 +0530

    fixed with solution

--- resources/js/Pages/Users.js
+++ resources/js/Pages/Users.js
@@ -2,30 +2,29 @@ import { Inertia } from "@inertiajs/inertia";
 import { usePage } from "@inertiajs/inertia-react";
 import React, { useEffect } from 'react'
 
+const removeListener = Inertia.on('navigate', () => {
+    Inertia.reload({ only: ['users'], onFinish: removeListener })
+})
+
+
 const Users = () => {
     const { users } = usePage().props;
 
+    // https://discord.com/channels/592327939920494592/592327939920494594/922476529399783534 sol reply on inertia support server :)
+
     useEffect(() => {
-        Inertia.reload({ only: ['users'] });
-    }, [])
+        const removeListener = Inertia.on('navigate', () => {
+            Inertia.reload({ only: ['users'], onFinish: removeListener })
+        })
+    })
 
-
-    // solution i found but why not working directly
-
-    /*
-    useEffect(() => {
-        setTimeout(() => {
-            Inertia.reload({ only: ['users'] });
-        });
-    }, [])
-    */
     return (
         <div>
             <p>
                 note: if you came here by previous link you will see the users data in console log but try reloading the page it will go in infinite loop
             </p>
             <h1>Check Console for users data!</h1>
-            {console.log(users)}
+            {users && console.log(users)}
         </div>
     )
 }
